0dfc0f99f83b312c4c239010cd964b36bbe81aa4
Ilia <daylayareli.dm@gmail.com>
2025-07-09 21:13:46 -0600

    state machine

--- client/src/game-objects/player/player.ts
+++ client/src/game-objects/player/player.ts
@@ -4,6 +4,10 @@ import { Position } from "../../common/types";
 import { InputComponent } from '../../components/input/input-component';
 import { ControlsComponent } from '../../components/game-object/controls-component';
 import { isArcadePhysicsBody } from '../../common/utils';
+import { StateMachine } from '../../components/state-machine/state-machine';
+import { IddleState } from '../../components/state-machine/states/idle-state';
+import { CHARACTER_STATES } from '../../components/state-machine/states/character-states';
+import { MoveState } from '../../components/state-machine/states/move-state';
 
 export type PlayerConfig = {
     scene: Phaser.Scene;
@@ -15,6 +19,8 @@ export type PlayerConfig = {
 
 export class Player extends Phaser.Physics.Arcade.Sprite {
     #controlsComponent: ControlsComponent;
+    #stateMachine: StateMachine;
+
     constructor(config: PlayerConfig){
         const {scene, position, assetKey, frame} = config;
         const { x, y } = position;
@@ -25,64 +31,25 @@ export class Player extends Phaser.Physics.Arcade.Sprite {
 
         this.#controlsComponent = new ControlsComponent(this, config.controls)
 
-        this.play({key: PLAYER_ANIMATION_KEYS.IDLE_DOWN, repeat: -1});
+        this.#stateMachine = new StateMachine('player');
+        this.#stateMachine.addState(new IddleState(this));
+        this.#stateMachine.addState(new MoveState(this));
+        this.#stateMachine.setState(CHARACTER_STATES.IDLE_STATE);
 
         config.scene.events.on(Phaser.Scenes.Events.UPDATE, this.update, this);
         config.scene.events.once(Phaser.Scenes.Events.SHUTDOWN, ()=> {
             config.scene.events.off(Phaser.Scenes.Events.UPDATE, this.update, this);
         });
     }
+    
+    get controls(): InputComponent {
+        return this.#controlsComponent.controls
+    }
 
     update(): void{
-        const controls = this.#controlsComponent.controls
-        if(controls.isUpDown){
-            this.play({key: PLAYER_ANIMATION_KEYS.WALK_UP, repeat: -1}, true);
-            this.#updateVelocity(false, -1);
-        } else if (controls.isDownDown){
-            this.play({key: PLAYER_ANIMATION_KEYS.WALK_DOWN, repeat: -1}, true);
-            this.#updateVelocity(false, 1);
-        } else {
-            this.#updateVelocity(false, 0);
-        }
-         
-        const isMovingVertically = controls.isDownDown || controls.isUpDown;
-        if(controls.isLeftDown){
-            this.setFlipX(true);
-            this.#updateVelocity(true, -1);
-            if(!isMovingVertically){
-                this.play({key: PLAYER_ANIMATION_KEYS.WALK_SIDE, repeat: -1}, true);
-            }
-        } else if (controls.isRightDown){
-            this.setFlipX(false);
-            this.#updateVelocity(true, 1);
-            if(!isMovingVertically){
-                this.play({key: PLAYER_ANIMATION_KEYS.WALK_SIDE, repeat: -1}, true);
-            }
-        } else {
-            this.#updateVelocity(true, 0);
-        }
 
-        if(!controls.isDownDown && !controls.isUpDown && !controls.isLeftDown && !controls.isRightDown){
-            this.play( {key: PLAYER_ANIMATION_KEYS.IDLE_DOWN, repeat: -1}, true)
-        }
-
-        this.#normalizeVelocity();
+        this.#stateMachine.update();
     }
 
-    #updateVelocity(isX: boolean, value: number): void{
-        if(!isArcadePhysicsBody(this.body)){
-            return;
-        }
-        if(isX){
-            this.body.velocity.x = value * 80;
-            return;
-        } this.body.velocity.y = value * 80;
-    }
 
-    #normalizeVelocity(): void{
-        if(!isArcadePhysicsBody(this.body)){
-            return;
-        }
-        this.body.velocity.normalize().scale(80);
-    }
 }
